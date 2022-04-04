b47e466b8e11371cf06373758c208bef872830e1
Sam Liu <samgliu19@gmail.com>
2022-04-03 22:58:46 -0400

    feat: adding Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import Headers from './Header';
+
+let renderCount = 0;
 
 function App() {
+  renderCount++;
+
   return (
     <div className="App">
+      <Headers
+        renderCount={renderCount}
+        description="Performant, flexible and extensible forms with easy-to-use validation."
+      />
     </div>
   );
 }
