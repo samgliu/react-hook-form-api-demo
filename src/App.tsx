0347685131fb543bb1fd253a791a6178d0d9e698
Sam Liu <samgliu19@gmail.com>
2022-04-03 20:29:16 -0400

    feat: register

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useForm } from 'react-hook-form';
+import Headers from './Header';
 
-function App() {
+let renderCount = 0;
+
+type FormValues = {
+  firstName: string;
+  lastName: string;
+  age: number;
+};
+
+export default function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({
+    defaultValues: {
+      firstName: '',
+      lastName: '',
+      age: 0,
+    },
+  });
+  renderCount++;
+
+  console.log('errors', errors);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Headers
+        renderCount={renderCount}
+        description="Performant, flexible and extensible forms with easy-to-use validation."
+      />
+      <form onSubmit={handleSubmit((data) => console.log(data))}>
+        <input
+          {...register('firstName', {
+            required: { value: true, message: 'This is required' },
+          })}
+          name="firstName"
+          placeholder="First Name"
+        />
+        <input
+          {...register('lastName', {
+            maxLength: { value: 5, message: 'Max length is 5' },
+          })}
+          name="lastName"
+          placeholder="Last Name"
+        />
+        <input type="number" {...register('age', { valueAsNumber: true })} />
+        <input type="submit" />
+      </form>
     </div>
   );
 }
-
-export default App;
