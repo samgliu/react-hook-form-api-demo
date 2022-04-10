d7cf4ac8fa6f7c43c9b61ca0b8920804f1c61472
Sam Liu <samgliu19@gmail.com>
2022-04-09 21:21:15 -0400

    feat: adding formState

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,45 @@
 import Headers from './Header';
-
+import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
 let renderCount = 0;
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState,
+    formState: {
+      errors,
+      isDirty,
+      dirtyFields,
+      touchedFields,
+      isSubmitted,
+      isSubmitSuccessful,
+      submitCount,
+      isValid,
+    },
+  } = useForm({
+    defaultValues: {
+      firstName: '',
+      lastName: '',
+    },
+  });
+
+  const onSubmit = (data: any) => {
+    console.log('data', data);
+  };
+
   renderCount++;
+
+  console.log('errors', errors);
+  console.log('isDirty', isDirty);
+  console.log('dirtyFields', dirtyFields);
+  console.log('touchedFields', touchedFields);
+  console.log('isSubmitSuccessful', isSubmitSuccessful);
+
+  useEffect(() => {
+    console.log('useEffect formstate', formState.errors);
+  }, [formState]);
 
   return (
     <div className="App">
@@ -11,6 +47,11 @@ function App() {
         renderCount={renderCount}
         description="Performant, flexible and extensible forms with easy-to-use validation."
       />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register('firstName', { required: true })} />
+        <input {...register('lastName', { required: true })} />
+        <input type="submit" />
+      </form>
     </div>
   );
 }
